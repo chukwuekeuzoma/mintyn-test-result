032f333028e2d75c245882dc794ecc277154d51d
mac <chukwuekeuzoma@gmail.com>
2022-11-17 21:02:30 +0100

    created allpayment data

---
+++ src/Data/Allpaymentdata.ts
@@ -0,0 +1,38 @@
+export const Allpaymentsdata = [
+   {
+     country:"Nigeria",
+     price:"4000" ,
+     transactionNo:"1234567",
+     time:"12:15pm",
+     status:"true"
+   },
+   {
+    country:"Togo",
+    price:"2000" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:"false"
+  },
+  {
+    country:"Niger",
+    price:"1000" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:"true"
+  },
+  {
+    country:"USA",
+    price:"500" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:"false"
+  },
+  {
+    country:"Canada",
+    price:"300" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:"true"
+  }
+]
+   

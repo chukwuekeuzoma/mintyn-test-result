c0daeb45c14f172ec1be97d98dd128aa66c82951
mac <chukwuekeuzoma@gmail.com>
2022-11-17 01:33:57 +0100

    added jsx

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Header.scss";
+import SearchInput from "../SearchInput/SearchInput.tsx";
+import Bell from "../../Asset/Svg/bell-53.svg";
+import Uzoma from "../../Asset/Img/Uzoma.jpg";
+
+export default function Header() {
+  const [terms, setTerms] = useState();
+
+  return (
+    <div className="header-main-container">
+      <div className="header-main-container-one">
+        <div className="trans-class">TransMonitor</div>
+        <div className="SearchInput-container">
+          <SearchInput
+            text="Search..."
+            getValue={(e) => setTerms(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="header-main-container-two">
+        <div className="support">Support</div>
+        <div className="faq">FAQ</div>
+        <div className="bell-container">
+          <div className="color-circle">3</div>
+          <img src={Bell} alt="bell-icon" />
+        </div>
+        <div className="profile-name-container">
+          <div>Hello</div>
+          <div>Oluwaleke Ojo</div>
+        </div>
+        <div className="header-profile-container">
+          <img src={Uzoma} alt="profile-pic" className="profile-pic" />
+        </div>
+      </div>
+    </div>
+  );
+}

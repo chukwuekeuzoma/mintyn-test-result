aa7aa63204da6c4ed390587ef3a6a8824f1ba807
mac <chukwuekeuzoma@gmail.com>
2022-11-18 08:47:58 +0100

    added both reconcile && unreconcile pages

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,5 +1,9 @@
 import Allpayments from "./Allpayments/Allpayments.tsx";
+import ReconcilePayment from "./ReconcilePayment/ReconcilePayment.tsx";
+import UnReconcilePayment from "./UnReconcilePayment/UnReconcilePayment.tsx";
 
 export {
-    Allpayments
+    Allpayments,
+    ReconcilePayment,
+    UnReconcilePayment
 }

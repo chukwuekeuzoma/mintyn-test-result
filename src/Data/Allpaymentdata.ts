807376c0e6a08f93691b15967f625c78f57ab8b9
mac <chukwuekeuzoma@gmail.com>
2022-11-18 08:45:49 +0100

    added extra data

--- src/Data/Allpaymentdata.ts
+++ src/Data/Allpaymentdata.ts
@@ -33,6 +33,41 @@ export const Allpaymentsdata = [
     transactionNo:"1234567",
     time:"12:15pm",
     status:1
+  },
+  {
+    country:"London",
+    price:"200" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:1
+  },
+  {
+    country:"Algeria",
+    price:"300" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:0
+  },
+  {
+    country:"Afghanistan",
+    price:"300" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:1
+  },
+  {
+    country:"Australia",
+    price:"300" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:0
+  },
+  {
+    country:"Cyprus",
+    price:"500" ,
+    transactionNo:"1234567",
+    time:"12:15pm",
+    status:1
   }
 ]
    

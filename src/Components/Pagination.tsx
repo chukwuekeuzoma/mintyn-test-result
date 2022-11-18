8d4d97271516921c576ae289031be231268e6375
mac <chukwuekeuzoma@gmail.com>
2022-11-18 06:57:42 +0100

    added pagination component

---
+++ src/Components/Pagination.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface Props {
+  postPerPage: any;
+  totalPosts: number;
+  paginate: (number: number) => number;
+}
+
+function Pagination({ postPerPage, totalPosts, paginate }: Props) {
+  const pageNumbers: number[] = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div>
+      {pageNumbers.map((number, key) => (
+        <div key={key} style={{ cursor: "pointer" }}>
+          <p onClick={() => paginate(number)}>{number}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Pagination;
